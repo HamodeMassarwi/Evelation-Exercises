6c995fa43a9acdf18d668771edbdcf813e7767c1
HamodeMassarwi <87018159+HamodeMassarwi@users.noreply.github.com>
2022-04-18 10:45:31 +0300

    week2

--- week2/Reservations/main.js
+++ week2/Reservations/main.js
@@ -2,4 +2,19 @@ const reservations = {
     Bob: { claimed: false },
     Ted: { claimed: true }
   }
+  const checkReservation = function(){
+    let typed = document.getElementById("stop").value;
+    let lowerCase = typed.toLowerCase();
+    const name = lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
+    if(reservations[name] && !(reservations[name].claimed)){
+      document.getElementById("header").innerHTML = "Welcome in ," + name ;
+    }else if(reservations[name] && reservations[name].claimed){
+      document.getElementById("header").innerHTML = name+ " Someone Already claimed this,! ";
+    }else{
+      document.getElementById("header").innerHTML = "You Have no reservations " + name ;
+    }
+
+
+
+  }
   
