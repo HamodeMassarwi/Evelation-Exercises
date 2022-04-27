9d7beb216905d65c298a398c7f6dacde1dbe1113
HamodeMassarwi <87018159+HamodeMassarwi@users.noreply.github.com>
2022-04-27 16:14:52 +0300

    h

---
+++ week3/Local Storage/main.js
@@ -0,0 +1,14 @@
+let userStorage = JSON.stringify({
+    darkMode: true,
+    showSideNav: false,
+    defaultResultCount: 9,
+    latestMarks: {
+        sectionA: 92,
+        sectionB: 11
+    },
+    cart: [
+        { id: 3112, count: 2 },
+        { id: 812, count: 16 }
+    ]
+})
+localStorage.userStorage = userStorage;
